a1efec801e898789b19ed8e57973d1a32e2779c4
unknown <shantanupawar101@gmail.com>
2024-02-06 02:11:46 +0530

    Added ability to draw and download

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -1,15 +1,85 @@
-const { useRef, useEffect, createContext } = require("react");
+const { useRef, useEffect, createContext, useLayoutEffect } = require("react");
+import { useSelector, useDispatch } from "react-redux";
+import { MENU_ITEMS } from "@/constants";
+import { actionItemClick } from "@/slice/menuSlice";
 
 const Board = () => {
   const canvasRef = useRef(null);
+  const dispatch = useDispatch();
+  const { activeMenuItem, actionMenuItem } = useSelector((state) => state.menu);
+  const { color, size } = useSelector((state) => state.toolbox[activeMenuItem]);
+  const shouldDraw = useRef(false);
 
   useEffect(() => {
-    if (!canvasRef) return;
+    if (!canvasRef.current) return;
     const canvas = canvasRef.current;
-    const context = canvas.getContext("2D");
+    const context = canvas.getContext("2d");
+
+    if (actionMenuItem === MENU_ITEMS.DOWNLOAD) {
+      const URL = canvas.toDataURL();
+      const anchor = document.createElement("a");
+      anchor.href = URL;
+      anchor.download = "sketch.jpg";
+      anchor.click();
+    }
+
+    dispatch(actionItemClick(null))
+  }), [actionMenuItem, dispatch];
+
+  useEffect(() => {
+    if (!canvasRef.current) return;
+    const canvas = canvasRef.current;
+    const context = canvas.getContext("2d");
+
+    const changeConfig = (color, size) => {
+      context.strokeStyle = color;
+      context.lineWidth = size;
+    };
+
+    changeConfig(color, size);
+  }, [color, size]);
+
+  useLayoutEffect(() => {
+    if (!canvasRef.current) return;
+    const canvas = canvasRef.current;
+    const context = canvas.getContext("2d");
 
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
+
+    const beginPath = (x, y) => {
+      context.beginPath();
+      context.moveTo(x, y);
+    };
+
+    const drawLine = (x, y) => {
+      context.lineTo(x, y);
+      context.stroke();
+    };
+
+    const handleMouseDown = (e) => {
+      shouldDraw.current = true;
+      beginPath(e.clientX, e.clientY);
+    };
+
+    const handleMouseMove = (e) => {
+      if (!shouldDraw.current) return;
+      drawLine(e.clientX, e.clientY);
+    };
+
+    const handleMouseUp = (e) => {
+      shouldDraw.current = false;
+    };
+
+    canvas.addEventListener("mousedown", handleMouseDown);
+    canvas.addEventListener("mousemove", handleMouseMove);
+    canvas.addEventListener("mouseup", handleMouseUp);
+
+    return () => {
+      canvas.removeEventListener("mousedown", handleMouseDown);
+      canvas.removeEventListener("mousemove", handleMouseMove);
+      canvas.removeEventListener("mouseup", handleMouseUp);
+    };
   }, []);
 
   return <canvas ref={canvasRef}></canvas>;
--- src/components/Toolbox/index.js
+++ src/components/Toolbox/index.js
@@ -2,17 +2,26 @@ import { useDispatch, useSelector } from "react-redux";
 import styles from "./index.module.css";
 import { COLORS } from "@/constants";
 import { MENU_ITEMS } from "@/constants";
+import { changeBrushSize, changeColor } from "@/slice/toolboxSlice";
+import cx from "classnames";
 
 const Toolbox = () => {
   const dispatch = useDispatch();
 
   const activeMenuItem = useSelector((state) => state.menu.activeMenuItem);
+  const { color, size } = useSelector((state) => state.toolbox[activeMenuItem]);
   const showStrokeToolOption = activeMenuItem === MENU_ITEMS.PENCIL;
   const showBrushToolOption =
     activeMenuItem === MENU_ITEMS.PENCIL ||
     activeMenuItem === MENU_ITEMS.ERASER;
 
-  const updateBrushSize = (e) => {};
+  const updateBrushSize = (e) => {
+    dispatch(changeBrushSize({ item: activeMenuItem, size: e.target.value }));
+  };
+
+  const updateColor = (newColor) => {
+    dispatch(changeColor({ item: activeMenuItem, color: newColor }));
+  };
 
   return (
     <div className={styles.toolboxContainer}>
@@ -21,51 +30,64 @@ const Toolbox = () => {
           <h4 className={styles.toolText}>Stroke Color</h4>
           <div className={styles.itemContainer}>
             <div
-              className={styles.colorBox}
+              className={cx(styles.colorBox, {
+                [styles.active]: color === COLORS.BLACK,
+              })}
               style={{ backgroundColor: COLORS.BLACK }}
               onClick={() => updateColor(COLORS.BLACK)}
             />
             <div
-              className={styles.colorBox}
+              className={cx(styles.colorBox, {
+                [styles.active]: color === COLORS.RED,
+              })}
               style={{ backgroundColor: COLORS.RED }}
               onClick={() => updateColor(COLORS.RED)}
             />
             <div
-              className={styles.colorBox}
+              className={cx(styles.colorBox, {
+                [styles.active]: color === COLORS.GREEN,
+              })}
               style={{ backgroundColor: COLORS.GREEN }}
               onClick={() => updateColor(COLORS.GREEN)}
             />
             <div
-              className={styles.colorBox}
+              className={cx(styles.colorBox, {
+                [styles.active]: color === COLORS.BLUE,
+              })}
               style={{ backgroundColor: COLORS.BLUE }}
               onClick={() => updateColor(COLORS.BLUE)}
             />
             <div
-              className={styles.colorBox}
+              className={cx(styles.colorBox, {
+                [styles.active]: color === COLORS.ORANGE,
+              })}
               style={{ backgroundColor: COLORS.ORANGE }}
               onClick={() => updateColor(COLORS.ORANGE)}
             />
             <div
-              className={styles.colorBox}
+              className={cx(styles.colorBox, {
+                [styles.active]: color === COLORS.YELLOW,
+              })}
               style={{ backgroundColor: COLORS.YELLOW }}
               onClick={() => updateColor(COLORS.YELLOW)}
             />
           </div>
         </div>
       )}
-      { showBrushToolOption &&
-      <div className={styles.toolItem}>
-        <h4 className={styles.toolText}>Brush Size</h4>
-        <div className={styles.itemContainer}>
-          <input
-            type="range"
-            min={1}
-            max={10}
-            step={1}
-            onChange={updateBrushSize}
-          />
+      {showBrushToolOption && (
+        <div className={styles.toolItem}>
+          <h4 className={styles.toolText}>Brush Size</h4>
+          <div className={styles.itemContainer}>
+            <input
+              type="range"
+              min={1}
+              max={10}
+              step={1}
+              onChange={updateBrushSize}
+            />
+          </div>
         </div>
-      </div>}
+      )}
     </div>
   );
 };
