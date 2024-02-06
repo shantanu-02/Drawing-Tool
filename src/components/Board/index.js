3a490865d83630c45466f24b53bd01a4a0abe3ac
unknown <shantanupawar101@gmail.com>
2024-02-06 15:02:31 +0530

    Added functionality to UNDO and REDO

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -6,6 +6,8 @@ import { actionItemClick } from "@/slice/menuSlice";
 const Board = () => {
   const canvasRef = useRef(null);
   const dispatch = useDispatch();
+  const drawHistory = useRef([]);
+  const historyPointer = useRef(0);
   const { activeMenuItem, actionMenuItem } = useSelector((state) => state.menu);
   const { color, size } = useSelector((state) => state.toolbox[activeMenuItem]);
   const shouldDraw = useRef(false);
@@ -21,10 +23,21 @@ const Board = () => {
       anchor.href = URL;
       anchor.download = "sketch.jpg";
       anchor.click();
+    } else if (
+      actionMenuItem === MENU_ITEMS.UNDO ||
+      actionMenuItem === MENU_ITEMS.REDO
+    ) {
+      if (historyPointer.current > 0 && actionMenuItem === MENU_ITEMS.UNDO)
+        historyPointer.current -= 1;
+      if (historyPointer.current < drawHistory.current.length - 1 && actionMenuItem === MENU_ITEMS.REDO)
+        historyPointer.current += 1;
+      const imageData = drawHistory.current[historyPointer.current];
+      context.putImageData(imageData, 0, 0);
     }
 
-    dispatch(actionItemClick(null))
-  }), [actionMenuItem, dispatch];
+    dispatch(actionItemClick(null));
+  }),
+    [actionMenuItem, dispatch];
 
   useEffect(() => {
     if (!canvasRef.current) return;
@@ -69,6 +82,9 @@ const Board = () => {
 
     const handleMouseUp = (e) => {
       shouldDraw.current = false;
+      const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+      drawHistory.current.push(imageData);
+      historyPointer.current = drawHistory.current.length - 1;
     };
 
     canvas.addEventListener("mousedown", handleMouseDown);
